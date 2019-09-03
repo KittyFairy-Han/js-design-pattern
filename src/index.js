6a49624212169df8c5ddd9eb10a813b1087b1265
fairyHan <1170189518@qq.com>
2019-09-04 06:32:50 +0800

    面向对象举例和介绍

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-class Person {
-    constructor(name){
-        this.name = name
+class
+JQuery
+{
+  constructor(selector)
+  {
+    let slice = Array.prototype.slice
+    let doms = slice.call(document.querySelectorAll(selector))
+    let len = doms ? doms.length : 0
+    for (let i = 0; i < len; i++) {
+      this[i] = doms[i]
     }
-    getName() {
-        return this.name
-    }
-}
+    this.length = len
+    this.selector = selector || ''
+  }
+  append(node)
+  {
 
-let p = new Person('yuxiaorou')
-alert(p.getName())
+  }
+}
+window.$ = function (s) {
+  return new JQuery(s)
+}
+//test
+console.log($('p'))
