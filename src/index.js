51778e7ed09f7c913f5258547f3ec7addd786506
fairyHan <1170189518@qq.com>
2019-09-04 07:53:03 +0800

    uml类图

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
-class
-JQuery
-{
-  constructor(selector)
-  {
-    let slice = Array.prototype.slice
-    let doms = slice.call(document.querySelectorAll(selector))
-    let len = doms ? doms.length : 0
-    for (let i = 0; i < len; i++) {
-      this[i] = doms[i]
-    }
-    this.length = len
-    this.selector = selector || ''
+class House {
+  constructor(city){
+    this.city = city
   }
-  append(node)
-  {
+  getCity(){
+    console.log(`house in ${this.city}`)
+  }
+}
+class Person {
+  constructor(name,house) {
+    this.name = name
+    this.house = house
+  }
+  saySomething(){
+    console.log(`my name is ${this.name} , I live in ${this.house.city}`)
+  }
+}
+class Student extends Person{
+   constructor(name, house, gradeClass) {
+     super(name,house)
+     this.gradeClass = gradeClass
+   }
+   saySomething() {
+     console.log(`I am an -${this.gradeClass}- student , my name is ${this.name}`)
+    //  this.house.getCity()//报错
+   }
+}
 
+class Teacher extends Person {
+  constructor(name, house, subject) {
+    super(name, house)
+    this.subject = subject
+  }
+  saySomething() {
+    console.log(`I teach -${this.subject}-  , my name is ${this.name} , I live in ${this.house.city}`) //this.house.city是undefined
   }
 }
-window.$ = function (s) {
-  return new JQuery(s)
-}
+
 //test
-console.log($('p'))
+const SA = new Student('小喵','萧山','大三')
+const TA = new Teacher('秋名', '滨江', '大学英语')
+SA.saySomething()
+TA.saySomething()
