dd55afeaf396054ac3cc508727190f4f3d8cb666
fairyHan <1170189518@qq.com>
2019-09-05 07:14:47 +0800

    1-5节 设计原则 用promise作为demo

--- src/index.js
+++ src/index.js
@@ -1,43 +1,24 @@
-class House {
-  constructor(city){
-    this.city = city
-  }
-  getCity(){
-    console.log(`house in ${this.city}`)
-  }
-}
-class Person {
-  constructor(name,house) {
-    this.name = name
-    this.house = house
-  }
-  saySomething(){
-    console.log(`my name is ${this.name} , I live in ${this.house.city}`)
-  }
-}
-class Student extends Person{
-   constructor(name, house, gradeClass) {
-     super(name,house)
-     this.gradeClass = gradeClass
-   }
-   saySomething() {
-     console.log(`I am an -${this.gradeClass}- student , my name is ${this.name}`)
-    //  this.house.getCity()//报错
-   }
+function loadImg(src) {
+  let promise = new Promise((resolve, reject) => {
+    let img = document.createElement('img')
+    img.onload = () => {
+      resolve(img)
+
+    }
+    img.onerror = () => {
+      reject('图片加载失败')
+    }
+    img.src = src
+  })
+  return promise
 }
 
-class Teacher extends Person {
-  constructor(name, house, subject) {
-    super(name, house)
-    this.subject = subject
-  }
-  saySomething() {
-    console.log(`I teach -${this.subject}-  , my name is ${this.name} , I live in ${this.house.city}`) //this.house.city是undefined
-  }
-}
-
-//test
-const SA = new Student('小喵','萧山','大三')
-const TA = new Teacher('秋名', '滨江', '大学英语')
-SA.saySomething()
-TA.saySomething()
+let result = loadImg('https://www.baidu.com/img/bd_logo1.png?qua=high')
+result.then((res) => {
+  console.log('part1', res)
+  return res
+}).then((res) => {
+  console.log('part2', `img-width:${res.width}`, `img-height:${res.height}`)
+}).catch((err) => {
+  console.log(err)
+})
