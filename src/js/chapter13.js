201f4a2074b1436eda3033d4d2eb05015c50aa4b
fairyHan <1170189518@qq.com>
2019-10-15 08:44:36 +0800

    组合模式，享元模式

--- src/js/chapter13.js
+++ src/js/chapter13.js
@@ -2,7 +2,7 @@
  * @Author: 鱼小柔 
  * @Date: 2019-10-11 08:23:02 
  * @Last Modified by: 鱼小柔
- * @Last Modified time: 2019-10-12 08:50:51
+ * @Last Modified time: 2019-10-15 08:36:25
  */
 
 /* 命令模式 */
@@ -115,15 +115,128 @@ payer.setNumber(100)
 console.log(a.number, b.number)
 
 /* 策略模式 */
+// 不同类型用户执行不同策略，直接拆分成多个类处理，就不用大量的if else了
 console.log('%%%%%%%%%%%%%%%%%%%%%%---策略模式---%%%%%%%%%%%%%%%%%%%%%%%%%')
+class LowRank {
+    todo() {
+        console.log(`普通用户购买`)
+    }
+}
+class MidRank {
+    todo() {
+        console.log(`会员用户购买`)
+    }
+}
+class SuperRank {
+    todo() {
+        console.log(`vip 购买`)
+    }
+}
+// 测试代码
+const orUser = new LowRank()
+orUser.todo()
+const memeberUser = new MidRank()
+memeberUser.todo()
+const vip = new SuperRank()
+vip.todo()
 
-
-/* 组合模式（树形结构） */
-console.log('%%%%%%%%%%%%%%%%%%%%%%---组合模式---%%%%%%%%%%%%%%%%%%%%%%%%%')
 
 /* 桥接模式 */
+// 一个类拆分成两个类 再连接
 console.log('%%%%%%%%%%%%%%%%%%%%%%---桥接模式---%%%%%%%%%%%%%%%%%%%%%%%%%')
+/* class ColorShape {
+    yellowCircle(){
+        console.log('yellow circle')
+    }
+    redCircle() {
+        console.log('red circle')
+    }
+    yellowTri() {
+        console.log('yellow triangle')
+    }
+    redTri() {
+        console.log('red triangle')
+    }
+}
+// 测试代码
+let cs = new ColorShape()
+cs.yellowCircle()
+cs.redCircle()
+cs.yellowTri()
+cs.redTri() */
+class Color{
+    constructor(type){
+        this.type = type
+    }
+}
+class Shape {
+    constructor(type,color) {
+        this.type = type
+        this.color = color
+    }
+    draw() {
+        console.log(`${this.type} ${this.color}`)
+    }
+}
+//测试代码
+let red = new Color('red')
+let yellow = new Color('red')
+let circle = new Color('circle',red)
+let triangle = new Color('triangle', yellow)
+circle.draw()
+triangle.draw()
+
 /* 原型模式 */
 console.log('%%%%%%%%%%%%%%%%%%%%%%---原型模式---%%%%%%%%%%%%%%%%%%%%%%%%%')
+const prototypeObj = {
+    getName(){
+        return this.first+' '+this.last
+    }
+}
+//测试代码
+let x = Object.create(prototypeObj)
+x.first = 'A'
+x.last = 'B'
+alert(x.getName())
+
+/* 组合模式（树形结构） */
+// 整体和单个节点操作一致，整体和单个节点的数据结构一致 --是组合模式的特点
+console.log('%%%%%%%%%%%%%%%%%%%%%%---组合模式---%%%%%%%%%%%%%%%%%%%%%%%%%')
+const vitrulDom = {
+    tag: 'div',
+    attr: {
+        id: 'div1',
+        className: 'wrapper',
+    },
+    children: [{
+            tag: 'span',
+            attr: {
+
+            },
+            children: ['span-one']
+
+        },
+        {
+            tag: 'span',
+            attr: {
+
+            },
+            children: ['span-two']
+
+        }
+
+    ]
+
+}
+
 /* 享元模式 */
+// 共享数据，节省开销，js没有经典场景
 console.log('%%%%%%%%%%%%%%%%%%%%%%---享元模式---%%%%%%%%%%%%%%%%%%%%%%%%%')
+// 父级代理点击事件
+/* <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul> */
