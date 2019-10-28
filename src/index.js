a505b7a55501f4b12e31e1caf8cbe9490fdf15b7
fairyHan <1170189518@qq.com>
2019-10-28 22:44:46 +0800

    代理器模式

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import { DH_UNABLE_TO_CHECK_GENERATOR } from "constants"
+
 /*
  * @Author: 鱼小柔 
  * @Date: 2019-09-18 08:32:03 
  * @Last Modified by: 鱼小柔
- * @Last Modified time: 2019-09-19 08:02:47
+ * @Last Modified time: 2019-10-28 22:43:58
  */
 /* 
 模式概念 
@@ -13,23 +15,23 @@
 
 /* 使用 */
 class RealImage {
-  constructor(fileName) {
-    this.fileName = fileName
-  }
-  loadImage() {
-    console.log(`loading image : ${this.fileName}`)
-  }
-  display() {
-    console.log(`display image ${this.fileName}`)
-  }
+    constructor(fileName) {
+        this.fileName = fileName
+    }
+    loadImage() {
+        console.log(`loading image : ${this.fileName}`)
+    }
+    display() {
+        console.log(`display image ${this.fileName}`)
+    }
 }
 class ProxyImage {
-  constructor(fileName) {
-    this.image = new RealImage(fileName)
-  }
-  display() {
-    this.image.display()
-  }
+    constructor(fileName) {
+        this.image = new RealImage(fileName)
+    }
+    display() {
+        this.image.display()
+    }
 }
 const image = new ProxyImage('XXXYYY')
 image.display()
@@ -51,30 +53,30 @@ image.display()
 
 /* class */
 class Star {
-  constructor() {
-    this.name = 'ABC'
-    this.age = 25
-    this.phone = '13998903422'
-  }
+    constructor() {
+        this.name = 'ABC'
+        this.age = 25
+        this.phone = '13998903422'
+    }
 
 }
 let star = new Star()
 /* 代理经纪人 */
 let agent = new Proxy(star, {
-  get: function (target, key) {
-    if (key === 'phone') {
-      return 'agent : 15856781234'
+    get: function (target, key) {
+        if (key === 'phone') {
+            return 'agent : 15856781234'
+        }
+        return target[key]
+    },
+    set: function (target, key, val) {
+        if (key === 'pay' && val < 100000) {
+            console.log('报价太低,12w以上')
+            return true
+        }
+        target[key] = val
+        return true
     }
-    return target[key]
-  },
-  set: function (target, key, val) {
-    if (key === 'pay' && val < 100000) {
-      console.log('报价太低,12w以上')
-      return false
-    }
-    target[key] = val
-    return true
-  }
 })
 
 /* 测试代码 */
